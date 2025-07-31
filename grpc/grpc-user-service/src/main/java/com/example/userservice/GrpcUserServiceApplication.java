b637e444919aa7dbd1a27b872089592ee185aaf7
장정우 <jungwoo@lotte.net>
2025-07-31 15:21:27 +0900

    20250731 - 마이크로서비스 디자인 패턴 완벽 가이드 - gRPC 구현(user-service)

---
+++ grpc/grpc-user-service/src/main/java/com/example/userservice/GrpcUserServiceApplication.java
@@ -0,0 +1,38 @@
+package com.example.userservice;
+
+import java.time.Duration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GrpcUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrpcUserServiceApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate getRestTemplate() {
+        int TIMEOUT = 5000;
+
+        RestTemplate restTemplate = new RestTemplateBuilder()
+            .connectTimeout(Duration.ofMillis(TIMEOUT))
+            .readTimeout(Duration.ofMillis(TIMEOUT))
+            .build();
+
+        return restTemplate;
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder()
+    {
+        return new BCryptPasswordEncoder();
+    }
+
+}
---
+++ grpc/grpc-user-service/src/main/java/com/example/userservice/vo/ResponseOrder.java
@@ -0,0 +1,24 @@
+package com.example.userservice.vo;
+
+import lombok.Data;
+
+@Data
+public class ResponseOrder {
+    private String productId;
+    private Integer qty;
+    private Integer unitPrice;
+    private Integer totalPrice;
+    private String createdAt;
+
+    private String orderId;
+
+    public ResponseOrder(String orderId, String productId, Integer qty,
+                         Integer unitPrice, Integer totalPrice, String createdAt) {
+        this.orderId = orderId;
+        this.productId = productId;
+        this.qty = qty;
+        this.unitPrice = unitPrice;
+        this.totalPrice = totalPrice;
+        this.createdAt = createdAt;
+    }
+}
